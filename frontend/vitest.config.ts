d611ac4a013d760183dab6f9447410d9d71d69cf
Samuel Egbajie (MAJ0R) <49847624+CodePapi@users.noreply.github.com>
2026-02-08 12:04:46 +0100

    fix: fixed vite setup

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,16 +1,10 @@
-import { defineConfig, mergeConfig } from 'vitest/config';
-import viteConfig from './vite.config';
-import path from 'path';
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
-export default mergeConfig(viteConfig, defineConfig({
+export default defineConfig({
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
-    // setupFiles: './src/test/setup.ts',
-    alias: {
-      'react': path.resolve(__dirname, './node_modules/react'),
-      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
-      'react-router-dom': path.resolve(__dirname, './node_modules/react-router-dom'),
-    },
   },
-}));
+});
